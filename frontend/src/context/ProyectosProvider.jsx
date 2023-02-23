4ed9c04c837372b41e9a4c9abc7b824881ab1aa0
Oscar Diaz <os.diazl@alumnos.duoc.cl>
2023-02-23 10:58:40 -0300

    añadir colaborador

--- frontend/src/context/ProyectosProvider.jsx
+++ frontend/src/context/ProyectosProvider.jsx
@@ -12,6 +12,7 @@ const ProyectosProvider = ({ children }) => {
   const [modalFormularioTarea, setModalFormularioTarea] = useState(false);
   const [tarea, setTarea] = useState({});
   const [modalEliminarTarea, setModalEliminarTarea] = useState(false);
+  const [colaborador, setColaborador] = useState({});
 
   const navigate = useNavigate();
 
@@ -31,7 +32,10 @@ const ProyectosProvider = ({ children }) => {
         const { data } = await clienteAxios('/proyectos', config);
         setProyectos(data);
       } catch (error) {
-        console.log(error);
+        setAlerta({
+          msg: error.response.data.msg,
+          error: true,
+        });
       }
     };
 
@@ -296,6 +300,70 @@ const ProyectosProvider = ({ children }) => {
     }
   };
 
+  const submitColaborador = async (email) => {
+    try {
+      setCargando(true);
+      const token = localStorage.getItem('token');
+      if (!token) return;
+
+      const config = {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+      };
+
+      const { data } = await clienteAxios.post(
+        `/proyectos/colaboradores`,
+        { email },
+        config
+      );
+
+      setColaborador(data);
+      setAlerta({});
+    } catch (error) {
+      setAlerta({
+        msg: error.response.data.msg,
+        error: true,
+      });
+    } finally {
+      setCargando(false);
+    }
+  };
+
+  const agregarColaborador = async (email) => {
+    try {
+      const token = localStorage.getItem('token');
+      if (!token) return;
+
+      const config = {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+      };
+
+      const { data } = await clienteAxios.post(
+        `/proyectos/colaboradores/${proyecto?._id}`,
+        email,
+        config
+      );
+
+      setAlerta({
+        msg: data.msg,
+        error: false,
+      });
+
+      setColaborador({});
+      setAlerta({});
+    } catch (error) {
+      setAlerta({
+        msg: error.response.data.msg,
+        error: true,
+      });
+    }
+  };
+
   return (
     <ProyectosContext.Provider
       value={{
@@ -306,6 +374,7 @@ const ProyectosProvider = ({ children }) => {
         modalFormularioTarea,
         tarea,
         modalEliminarTarea,
+        colaborador,
 
         mostrarAlerta,
         submitProyecto,
@@ -316,6 +385,8 @@ const ProyectosProvider = ({ children }) => {
         handleModalEditarTarea,
         handleModalEliminarTarea,
         eliminarTarea,
+        submitColaborador,
+        agregarColaborador,
       }}
     >
       {children}
